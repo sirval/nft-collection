f3fe9d504d0d841c3544c30344bc99f91c08f111
Ikenna Valentine <ohukaiv@gmail.com>
2023-02-17 22:21:16 +0100

    added metadata to the nfts

--- my-app/pages/api/hello.js
+++ my-app/pages/api/hello.js
@@ -1,5 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const tokenId = req.query.tokenId;
+
+  console.error("error");
+  const name = `Valoski NFT #${tokenId}`;
+  const description =
+    "Valoski NFT is my first NFT in my course of learning web3 and Blockchain development";
+  const image = `https://raw.githubusercontent.com/sirval/nft-collection/main/my-app/public/images/${
+    Number(tokenId) - 1
+  }.svg`;
+
+  return res.status(200).json({
+    name: name,
+    description: description,
+    image: image,
+  });
 }
